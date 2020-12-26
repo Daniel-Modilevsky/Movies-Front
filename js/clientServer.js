06e025191ba2adef236955628fa4cc3966119d62
Daniel-Modilevsky <62332613+Daniel-Modilevsky@users.noreply.github.com>
2020-12-27 00:50:42 +0200

    movie page + home

--- js/clientServer.js
+++ js/clientServer.js
@@ -1,9 +1,29 @@
 /*global $ */
 /*jslint browser: true*/
 /*eslint no-console: "error"*/
+var movieID = '5fe392b1056d0822ecc9f3f8';
 
 $(function() {
+    //getMovies();
+    getComedy();
+    getAction();
+    getDrama();
+    //var movieI = sessionStorage.getItem('favoriteMovie');
+    getMovie(movieID);
 });
+
+
+function sendIdToMovie(idMovie){
+    window.name.movieID = idMovie;
+    console.log(movieID);
+    //getMovie(movieID);
+    top.location.href="movie.html";
+   // sessionStorage.setItem("favoriteMovie", movieID);
+
+    //window.location.href = 'movie.html' + '#' + movieID;
+
+}
+
 
 function getIndex(){
     $.ajax({
@@ -31,10 +51,9 @@ function postLogin(){
             dataType : 'json'
         })
         .done(function(message) {
-            alert(message);
             console.log(`Success - Login - ${message}`);
             //window.location = 'temp2.html';
-            top.location.href="homepage.html"
+            top.location.href="home.html"
         })
         .fail(function(jqXHR, textStatus, message){  
             alert(`Error - Login - ${textStatus} ,  ${message}`); 
@@ -57,10 +76,9 @@ function postRegister(){
             dataType : 'json'
         })
         .done(function(message) {
-            alert(message);
             console.log(`Success - Register - ${message}`);
             //window.location = 'temp2.html';
-            top.location.href="homepage.html"
+            top.location.href="home.html"
         })
         .fail(function(jqXHR, textStatus, message){  
             alert(`Error - Register - ${textStatus} ,  ${message}`); 
@@ -72,6 +90,130 @@ function getListeners(){
  
 };
 
+/*function getMovies(){
+    $.ajax({
+        url: 'http://localhost:8080/api/movies',
+        type: 'GET',
+        success: function(movies) {
+            console.log(`Success - get - movies = ${movies.length}`);
+            $(".movies-list").empty();
+            movies.forEach(movie => {
+                $('.movies-list').append(
+            "<article class='movie-mini hvr-curl-top-right hvr-shrink'>" +
+            "<img src = '" +'http://localhost:8080/' + movie.image + "'>" +
+
+            "</article>"
+        );
+    });
+        },
+        error:function(){  
+           alert('Error - get - movies')  
+        }   
+    });
+}
+*/
+function getComedy(){
+    $.ajax({
+        url: 'http://localhost:8080/api/categiries/Comedy',
+        type: 'GET',
+        success: function(movies) {
+            console.log(`Success - get - movies = ${movies.length}`);
+            $(".list1").empty();
+            $(".list1").append("<h4>Comedy</h4>");
+            movies.forEach(movie => {
+                $('.list1').append(
+                '<article class="movie-mini hvr-curl-top-right hvr-shrink" onClick="sendIdToMovie(\'' + movie._id + '\')" >' +
+                "<img src = '" +'http://localhost:8080/' + movie.image + "'>" +
+                "</article>"
+        );
+    });
+        },
+        error:function(){  
+           alert('Error - get - movies')  
+        }   
+    });
+}
+
+function getAction(){
+    $.ajax({
+        url: 'http://localhost:8080/api/categiries/Action',
+        type: 'GET',
+        success: function(movies) {
+            console.log(`Success - get - movies = ${movies.length}`);
+            $(".list2").empty();
+            $(".list2").append("<h4>Action</h4>");
+            movies.forEach(movie => {
+                $('.list2').append(
+            "<article class='movie-mini hvr-curl-top-right hvr-shrink'>" +
+            "<img src = '" +'http://localhost:8080/' + movie.image + "'>" +
+
+            "</article>"
+        );
+    });
+        },
+        error:function(){  
+           alert('Error - get - movies')  
+        }   
+    });
+}
+
+function getDrama(){
+    $.ajax({
+        url: 'http://localhost:8080/api/categiries/Drama',
+        type: 'GET',
+        success: function(movies) {
+            console.log(`Success - get - movies = ${movies.length}`);
+            $(".list3").empty();
+            $(".list3").append("<h4>Drama</h4>");
+            movies.forEach(movie => {
+                $('.list3').append(
+            "<article class='movie-mini hvr-curl-top-right hvr-shrink'>" +
+            "<img src = '" +'http://localhost:8080/' + movie.image + "'>" +
+
+            "</article>"
+        );
+    });
+        },
+        error:function(){  
+           alert('Error - get - movies')  
+        }   
+    });
+}
+
+function getMovie(idOfMovie){
+    $.ajax({
+        url: `http://localhost:8080/api/movies/${idOfMovie}`,
+        type: 'GET',
+        success: function(movie) {
+            console.log('Success - movieID');
+            $("#movie-header").empty();
+            $("#movie-header").append(movie.movie.name +" - Movie");
+
+            $("#movie").empty();
+            $("#movie").append(
+                "<section id='movie-image'>" + 
+                "<img src='" +'http://localhost:8080/' + movie.movie.image + "'></section>"+
+                "<div id='movie-details'><navbar class='movie-left'><ul>"+
+                "<li><label>Name</label> : <span class='movie-name'>" + movie.movie.name + "</span></li>" +
+                "<li><label>Time</label> : <span>"+movie.movie.runTime+"</span></li>" +
+                " <li><label>Date</label> : <span>"+movie.movie.releaseDate+"</span></li>" +
+                "<li><label>Rate</label> : <span>"+7.4+"</span></li></ul></navbar>" +
+                "<aside class='movie-right'><ul>"+       
+                "<li><label>Categories</label> : <span>"+movie.movie.categories+"</span></li>"+
+                "<li><label>Actors</label> : <span>"+movie.movie.actors+"</span></li>"+ 
+                "<li><label>Writer</label> : <span>"+movie.movie.writer+"</span></li>"+ 
+                "<li><label>Director</label> : <span>"+movie.movie.director+"</span></li></ul></aside>"+ 
+                "<p><label>Story Line</label> : <span>"+movie.movie.storyline+"</span></p></div>"
+            );
+            
+        },
+        error:function(){  
+           alert('Error - index')  
+        }   
+    });
+}
+
+
 $(document).on('click', '#login-button', function(e){
     e.preventDefault();
     postLogin();
@@ -81,4 +223,9 @@ $(document).on('click', '#registerButton', function(e){
     e.preventDefault();
     postRegister();
 });
-
+/*
+$(document).on('click', '.movie-mini', function(e){
+    e.preventDefault();
+    sendIdToMovie();
+});
+*/
